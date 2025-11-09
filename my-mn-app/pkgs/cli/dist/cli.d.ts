82ced25262ea147cb76c81fab4a73187238de7ee
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2025-11-09 11:29:09 +0000

    テストネットにデプロイできるまで確認

--- my-mn-app/pkgs/cli/dist/cli.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Logger } from 'pino';
-import { type DockerComposeEnvironment } from 'testcontainers';
-import { type Config } from './config';
-export declare const run: (config: Config, _logger: Logger, dockerEnv?: DockerComposeEnvironment) => Promise<void>;
--- my-mn-app/pkgs/cli/dist/utils/common-types.d.ts
+++
@@ -1,8 +0,0 @@
-import { Counter, type CounterPrivateState } from 'contract';
-import type { ImpureCircuitId, MidnightProviders } from '@midnight-ntwrk/midnight-js-types';
-import type { DeployedContract, FoundContract } from '@midnight-ntwrk/midnight-js-contracts';
-export type CounterCircuits = ImpureCircuitId<Counter.Contract<CounterPrivateState>>;
-export declare const CounterPrivateStateId = "counterPrivateState";
-export type CounterProviders = MidnightProviders<CounterCircuits, typeof CounterPrivateStateId, CounterPrivateState>;
-export type CounterContract = Counter.Contract<CounterPrivateState>;
-export type DeployedCounterContract = DeployedContract<CounterContract> | FoundContract<CounterContract>;
